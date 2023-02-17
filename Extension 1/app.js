a79d5b980f66d1c1d4b844145f76c3893ac65ac0
anmoljain16 <75519593+anmoljain16@users.noreply.github.com>
2023-02-18 01:51:30 +0530

    Add files via upload

---
+++ Extension 1/app.js
@@ -0,0 +1,52 @@
+console.log("Heheheheh")
+
+console.log("Saifuling ")
+
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", async () => {
+    console.log("click");
+    let [tab] = await chrome.tabs.query({active: true, currentWindow : true});
+
+    chrome.scripting.executeScript({
+        target: {tabId : tab.id},
+        function : bakchodi,
+    });
+})
+
+function bakchodi() {
+    console.log("bakkkkchodi")
+    const x = document.getElementById('txtU')
+    x.value="12017878"
+
+    const y = document.getElementById('TxtpwdAutoId_8767')
+    y.value="i@Anmol123"
+
+    const btn = document.getElementById('iBtnLogins')
+    btn.click()
+}
+
+
+const placement = document.getElementById("placement")
+
+placement.addEventListener("click", async () => {
+    console.log("click");
+    let [tab] = await chrome.tabs.query({active: true, currentWindow : true});
+
+    chrome.scripting.executeScript({
+        target: {tabId : tab.id},
+        function : location.href = "https://ums.lpu.in/Placements/HomePlacementStudent.aspx",
+    });
+})
+//
+// function bakchodi() {
+//     console.log("bakkkkchodi")
+//     const x = document.getElementById('txtU')
+//     x.value="12017878"
+//
+//     const y = document.getElementById('TxtpwdAutoId_8767')
+//     y.value="i@Anmol123"
+//
+//     const btn = document.getElementById('iBtnLogins')
+//     btn.click()
+// }
